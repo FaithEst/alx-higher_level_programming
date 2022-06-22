4a8376be2fa19044bd7a236962005ec42e63dead
Faith <mutitufaith53@gmail.com>
2022-06-22 05:34:22 -0700

    adds task 4

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" This module defines a class 'Square'. """
+
+
 class Square:
-    """
-    Represents a square.
+    """Represents a square.
     Private instance attribute: size:
         - property def size(self)
         - property setter def size(self, value)
